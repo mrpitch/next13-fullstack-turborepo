9a07f44c54e0ef2886bbb527bf00d052f932df41
mrpitch <mrpitch@outlook.com>
2023-07-12 16:15:30 +0200

    feat: init setup: turborepo, tailwind, next13

---
+++ apps/my-app2/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-6 h-full w-full">
+      <h1 className="text-hurdi">Hurdi</h1>
+      <h2 className="text-gurdi">Gurdi</h2>
+    </main>
+  );
+}
