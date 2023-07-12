5f672298d53e74f8ca3426f6ab21212cf310727d
mrpitch <mrpitch@outlook.com>
2023-07-12 17:31:59 +0200

    feat: add shadcn ui as shared library

--- apps/my-app1/src/app/page.tsx
+++ apps/my-app1/src/app/page.tsx
@@ -1,8 +1,35 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "ui";
+
 export default function Home() {
   return (
-    <main className="p-6 h-full w-full">
-      <h1 className="text-hurdi">Hurdi</h1>
-      <h2 className="text-gurdi">Gurdi</h2>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2 className="text-4xl font-bold">my-app-1</h2>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other components
+            aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. Its animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </main>
   );
 }
