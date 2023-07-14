49db807c98ec8dba0b537dd69fe9b17023672e30
mrpitch <mrpitch@outlook.com>
2023-07-14 09:24:19 +0200

    fix: cleanup everything

--- packages/ui/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
-const sharedConfig = require("tailwind-config/tailwind.config.js");
-
-module.exports = {
-  ...sharedConfig,
-};
